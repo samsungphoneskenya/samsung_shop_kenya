32ba12d7616d392a123e47805735d25cc7ae0d87
mongare <mongaresams@gmail.com>
2026-02-05 23:43:32 +0300

    Update shop page with filters and fetch data dynamically

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -6,16 +6,27 @@ import FloatingButtons from "@/components/shop/FloatingButtons";
 import Hero from "@/components/shop/Hero";
 import NewArrivals from "@/components/shop/NewArrivals";
 import OnSale from "@/components/shop/OnSale";
+import {
+  getBestSellers,
+  getNewArrivals,
+  getOnSaleProducts,
+} from "@/lib/actions/storefront-actions";
 
-export default function Home() {
+export default async function Home() {
+  const [newArrivals, onSale, bestSellers] = await Promise.all([
+    getNewArrivals(8),
+    getOnSaleProducts(8),
+    getBestSellers(8),
+  ]);
+
   return (
     <div className="min-h-screen bg-white">
       <Header />
       <Hero />
       <CategoryBanner />
-      <NewArrivals />
-      <OnSale />
-      <Bestsellers />
+      <NewArrivals products={newArrivals} />
+      <OnSale products={onSale} />
+      <Bestsellers products={bestSellers} />
       <Footer />
       <FloatingButtons />
     </div>
