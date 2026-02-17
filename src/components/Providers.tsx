047369bf4a9e3ec74c838a4cd0880fcf90c170bb
mongare <mongaresams@gmail.com>
2026-02-17 23:27:32 +0300

    Update providers

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,11 +3,14 @@
 import { ReactNode } from "react";
 import { CartProvider } from "@/contexts/CartContext";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { FavouritesProvider } from "@/contexts/FavouritesContext";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <AuthProvider>
-      <CartProvider>{children}</CartProvider>
+      <FavouritesProvider>
+        <CartProvider>{children}</CartProvider>
+      </FavouritesProvider>
     </AuthProvider>
   );
 }
