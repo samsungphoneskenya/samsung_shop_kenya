9f67c275e1527b88332b96ef28e989e802c542f0
mongare <mongaresams@gmail.com>
2026-02-27 10:03:10 +0300

    Add two columns to products

--- src/components/shop/NewArrivals.tsx
+++ src/components/shop/NewArrivals.tsx
@@ -18,7 +18,7 @@ export default function NewArrivals({
           <p className="text-gray-600 text-lg">Browse our newest arrivals</p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-3 sm:gap-6">
           {products.map((product) => (
             <ProductCard
               key={product.id}
