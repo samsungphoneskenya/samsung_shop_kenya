70e8b4903437ebe80af11c6f54dcf908f242e367
mongare <mongaresams@gmail.com>
2026-03-01 12:34:04 +0300

    Add vercel speed and analytics insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ToastProvider } from "@/components/ui/use-toast";
 
 export const metadata: Metadata = {
@@ -17,6 +19,8 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-background text-foreground antialiased">
         <ToastProvider>{children}</ToastProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
